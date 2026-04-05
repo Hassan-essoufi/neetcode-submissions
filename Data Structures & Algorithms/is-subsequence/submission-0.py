3810e9d3e45f21f6ffa021580f4e4838c3960a6a
Sprezzatura <hassanessoufi2004@gmail.com>
2026-04-05 18:34:46 +0100

    Add: is-subsequence - submission-0

---
+++ Data Structures & Algorithms/is-subsequence/submission-0.py
@@ -0,0 +1,18 @@
+class Solution:
+    def isSubsequence(self, s: str, t: str) -> bool:
+        if len(s) == 0:
+            return True
+        if len(t) == 0:
+            return False
+        i = 0
+        j = 0
+        while i < len(s):
+            while j < len(t):
+                if s[i] == t[j]:
+                    i += 1
+                else:
+                    j +=1
+                if i == len(s):
+                    return True
+                if j == len(t):
+                    return False
