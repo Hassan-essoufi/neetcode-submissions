a95d8d70732f576886e49fe43ff675d524d1a811
Sprezzatura <hassanessoufi2004@gmail.com>
2026-04-07 10:04:22 +0100

    Add: valid-perfect-square - submission-7

---
+++ Data Structures & Algorithms/valid-perfect-square/submission-7.py
@@ -0,0 +1,16 @@
+class Solution:
+    def isPerfectSquare(self, num: int) -> bool:
+        if num == 1:
+            return True
+        l = [i for i in range(0,num)]
+        left = 0
+        right = num-1
+        while left <= right:
+            m = left + (right-left)//2
+            if l[m]*l[m] == num:
+                return True
+            elif l[m]*l[m] > num:
+                right -= 1
+            else:
+                left +=1
+        return False
