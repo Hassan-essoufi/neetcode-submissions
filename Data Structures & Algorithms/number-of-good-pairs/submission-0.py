c3f580e71ed6adb10e93f187145084224b4b5ef7
Sprezzatura <hassanessoufi2004@gmail.com>
2026-04-15 07:29:08 +0100

    Add: number-of-good-pairs - submission-0

---
+++ Data Structures & Algorithms/number-of-good-pairs/submission-0.py
@@ -0,0 +1,9 @@
+class Solution:
+    def numIdenticalPairs(self, nums: List[int]) -> int:
+        p = 0
+
+        for i in range(len(nums)):
+            for j in range(i+1,len(nums)):
+                if nums[i] == nums[j]:
+                    p += 1
+        return p
