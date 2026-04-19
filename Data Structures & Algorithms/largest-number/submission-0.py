f71c4f848052925b118a9ab3df149540fd420d81
Sprezzatura <hassanessoufi2004@gmail.com>
2026-04-19 03:43:04 +0100

    Add: largest-number - submission-0

---
+++ Data Structures & Algorithms/largest-number/submission-0.py
@@ -0,0 +1,18 @@
+class Solution:
+    def rest(self, target):
+        p = target
+        while target > 9:
+            p = target % 10
+            target = target // 10
+            
+        return p
+
+
+    def largestNumber(self, nums: List[int]) -> str:
+
+        for i  in range(len(nums)):
+            for j in range(i+1,len(nums)):
+                if self.rest(nums[i]) <= self.rest(nums[j]):
+                    nums[i], nums[j] = nums[j], nums[i]
+        result = "".join(map(str, nums))
+        return result
