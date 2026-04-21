e93d23822559c30d63add15e331bd78c98d595cb
Sprezzatura <hassanessoufi2004@gmail.com>
2026-04-21 06:45:41 +0100

    Add: string-encode-and-decode - submission-3

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-3.py
@@ -0,0 +1,13 @@
+class Solution:
+
+    def encode(self, strs: List[str]) -> str:
+
+        s = strs[0]
+        for i in strs[1:]:
+            s = s + '#' + i
+        return s
+    def decode(self, s: str) -> List[str]:
+        if len(s) == 0:
+            return [s]
+        l = s.split("#")
+        return l
