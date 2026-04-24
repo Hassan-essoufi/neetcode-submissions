f6a2fddc3328041de2c9d31fbadefd6f171ade58
Sprezzatura <hassanessoufi2004@gmail.com>
2026-04-24 19:04:09 +0100

    Add: arranging-coins - submission-1

---
+++ Data Structures & Algorithms/arranging-coins/submission-1.py
@@ -0,0 +1,10 @@
+class Solution:
+    def arrangeCoins(self, n: int) -> int:
+        p = []
+        rest = n-1
+        for i in range(1,n):
+            if i > rest:
+                return len(p)
+            p.append(i)
+            rest = rest -i
+        return len(p)
