cf6bab213e433c69fca913e1f6a9122899d230ff
Sprezzatura <hassanessoufi2004@gmail.com>
2026-04-26 18:19:11 +0100

    Add: last-stone-weight - submission-3

---
+++ Data Structures & Algorithms/last-stone-weight/submission-3.py
@@ -0,0 +1,16 @@
+class Solution:
+    def lastStoneWeight(self, stones: List[int]) -> int:
+        if len(stones) == 1:
+            return stones[0]
+        while len(stones) > 1:
+
+            y = max(stones)  
+            stones.remove(y)
+            x = max(stones)
+            if x == y:
+                stones.remove(x)
+            else:
+                i = stones.index(x)
+                stones [i] = y-x
+            
+        return stones[0] if len(stones) == 1 else 0
