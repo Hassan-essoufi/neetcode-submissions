4bd635731074c063ac423d710509ddfa0a3da12d
Sprezzatura <hassanessoufi2004@gmail.com>
2026-04-28 23:26:40 +0100

    Add: isomorphic-strings - submission-1

---
+++ Data Structures & Algorithms/isomorphic-strings/submission-1.py
@@ -0,0 +1,11 @@
+class Solution:
+    def isIsomorphic(self, s: str, t: str) -> bool:
+        mapping = {}
+        for i in range(len(s)):
+            if s[i] not in mapping:
+                mapping[s[i]] = t[i]
+                
+            else:
+                if mapping[s[i]] != t[i]:
+                    return False
+        return True
